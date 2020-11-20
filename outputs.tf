9de4eac93d4415da55e15fb7185ae3ab2290bca1
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-20 14:55:33 -0600

    Fixed output files, wasn't dynamic enought

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,11 @@
-# Output the instance public ip
-output "INSTANCE_PUBLIC_IP" {
-  value = aws_instance.webserver.public_ip
+# Output the public IP's of the worker nodes
+output "NODES_PUBLIC_IP" {
+    value = {
+        for instance in aws_instance.webserver:
+            instance.id => instance.public_ip
+    }
 }
 
-output "INSTANCE_ID" {
-  value = aws_instance.webserver.id
-}
+# output "INSTANCE_ID" {
+#   value = aws_instance.webserver.id
+# }
