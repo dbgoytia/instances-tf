7a376faf6a86c0557f054477f0488e09ffa91d4a
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-30 14:43:08 -0600

    Removed the output for the public ips.

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-# Output the public IP's of the worker nodes
-output "NODES_PUBLIC_IPS" {
-  value = {
-    for instance in aws_instance.webserver :
-    instance.id => instance.public_ip
-  }
-}
+# # Output the public IP's of the worker nodes
+# output "NODES_PUBLIC_IPS" {
+#   value = {
+#     for instance in aws_instance.webserver :
+#     instance.id => instance.public_ip
+#   }
+# }
